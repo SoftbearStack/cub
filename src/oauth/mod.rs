3634d39f757ccc495e57aaa9c31866f6692a0f98
Cory Bear <git@corybear.com>
2025-03-01 20:28:04 -0800

    0.1.13

--- src/oauth/mod.rs
+++ src/oauth/mod.rs
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: 2024 Softbear, Inc.
-// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-License-Identifier: LGPL-3.0-or-later
 
 /// A client of one or more OAuth2 provider APIs.
 mod client;
